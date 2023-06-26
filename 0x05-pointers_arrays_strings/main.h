acf1273f33e220ff15b55729c040c0d0839a1f0d
laabk <laabkdavid@gmail.com>
2023-06-26 07:31:47 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
-void _puts(char *str);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -14,5 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void swap_int(int *a, int *b);
-int _putchar(char c);
+
+#endif
