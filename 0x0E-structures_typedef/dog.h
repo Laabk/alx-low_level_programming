352cab3530a3005040b8f77fbf5a81145b02bc44
laabk <laabkdavid@gmail.com>
2023-07-17 10:13:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,9 @@
 
 typedef struct dog
 {
-        float age;
-        char *name, *owner;
+	float age;
+	char *name, *owner;
 } dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
