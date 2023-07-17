8a0c023836ccca340c85bd8bcd0dc17371b71f33
laabk <laabkdavid@gmail.com>
2023-07-17 09:56:41 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+
+/**
+ * struct dog - A new form that is to be used in describing
+ * the dog.
+ * @name: The dog's usual name.
+ * @age: The dogs years.
+ * @owner: Th one who owns dog.
+ */
+
+typedef struct dog
+{
+        float age;
+        char *name, *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
