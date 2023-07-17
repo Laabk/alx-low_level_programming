e839d3a650a5735840c57d0c93fd659ced4f2d01
laabk <laabkdavid@gmail.com>
2023-07-17 05:20:37 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+
+/**
+ * main: 1-pi.h
+ * descptn: creating header file that defines a macro named
+ * PI as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+
+#endif
