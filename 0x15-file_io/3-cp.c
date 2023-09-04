a1e82cd352f0bce8a1510931a32b0b126235e4a3
laabkd <laabkdavid@gmail.com>
2023-09-04 19:44:28 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,98 +1,99 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-
-char *create_cont(char *file);
-void close_file(int fld);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * create_cont - the reserve 1024 bytes for cont.
- * @file: name of the file
- * Return: the pointer
+ * create_buffer - this Allocates 1024 bytes for a container.
+ * @file: name of file container is storing chars for.
+ * Return: pointer to newly-allocated buffer.
  */
 
-char *create_cont(char *file)
+char *create_container(char *file)
 {
-	char *numb;
+	char *container;
 
-	numb = malloc(sizeof(char) * 1024);
+	container = malloc(sizeof(char) * 1024);
 
-	if (numb == NULL)
+	if (container == NULL)
 	{
-	dprintf(STDERR_FILENO,
-	"Error: Can't write to %s\n", file);
-	exit(99);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
 	}
 
-	return (numb);
+	return (container);
 }
 
 /**
- * close_file - this func exits the file descriptors.
- * @filn: file to be close
+ * close_file - this Closes file descriptors involved
+ * @fd: file descriptor to be closed.
  */
-
-void close_file(int fld)
+void close_file(int fd)
 {
 	int a;
 
-	a = close(fld);
+	a = close(fd);
 
 	if (a == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't close fld %d\n", fld);
-	exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 }
 
 /**
- * main - this Copies files to another
- * @argc: arguments involved in program.
- * @argv: ther pointers to the arguments involved
- * Return: 0 success
+ * main - this Copies the contents of a file to that of another file.
+ * @argc: number of arguments applied to the program.
+ * @argv: array of pointers to arguments.
+ * Return: 0 on success.
+ * Description: so if the argument count is incorrect - exit code 97.
  */
 
 int main(int argc, char *argv[])
 {
-	int a, z;
-	int n, i;
-	char *numb;
+	int from, to, a, b;
+	char *container;
 
 	if (argc != 3)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	numb = create_cont(argv[2]);
-	n = open(argv[1], O_RDONLY);
-	a = read(a, cont, 1024);
-	z = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	container = create_container(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	a = read(from, container, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-	if (n == -1 || a == -1)
-	{
-	dprintf(STDERR_FILENO,
-	"Error: Can't read from file %s\n", argv[1]);
-	free(numb);
-	exit(98);
-	}
+		if (from == -1 || a == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(container);
+			exit(98);
+		}
 
-	i = write(z, numb, a);
-	if (z == -1 || i == -1)
-	{
-	dprintf(STDERR_FILENO,
-	"Error: Can't write to %s\n", argv[2]);
-	free(numb);
-	exit(99);
-	}
+		b = write(to, container, a);
+		if (to == -1 || b == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(container);
+			exit(99);
+		}
 
-	a = read(n, numb, 1024);
-	z = open(argv[2], O_WRONLY | O_APPEND);
+		a = read(from, container, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (a > 0);
-	free(numb);
-	close_file(n);
-	close_file(z);
+
+	free(container);
+	close_file(from);
+	close_file(to);
+
 	return (0);
 }
